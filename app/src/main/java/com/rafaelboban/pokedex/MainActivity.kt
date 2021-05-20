967397f0c05ac1ea6d5d1aac1ca5b9b0471b1643
Phelenor <rafael.boban00@gmail.com>
2021-05-20 16:57:07 +0200

    Add custom action bars to fragments

--- app/src/main/java/com/rafaelboban/pokedex/MainActivity.kt
+++ app/src/main/java/com/rafaelboban/pokedex/MainActivity.kt
@@ -20,16 +20,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_search, R.id.navigation_favorites, R.id.navigation_settings
-            )
+            setOf(R.id.navigation_search, R.id.navigation_favorites, R.id.navigation_settings)
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        supportActionBar?.hide()
     }
 }
